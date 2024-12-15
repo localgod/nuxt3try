fccc24618364e87b3f57585138b0c13f0933edd0
Johannes Skov Frandsen <jsf@greenoak.dk>
2024-12-15 19:59:49 +0000

    try

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    Hej
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    Hej med dig
+    {{ matrikel1 }}
+</template>
+<script setup lang="ts">
+const matrikel1 = (await useFetch(() => '/api/dataforsyningen/jordstykker?ejerlavkode=211255&matrikelnr=16d')).data.value
+</script>
