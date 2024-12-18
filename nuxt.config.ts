59c963d458b44f7b154f6a00ba2d5131a92cedea
Johannes Skov Frandsen <jsf@greenoak.dk>
2024-12-18 10:52:02 +0000

    added sanity support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/leaflet', 'usebootstrap']
+  runtimeConfig: {
+    sanity: {
+      projectId: process.env.SANITY_PROJECT_ID,
+      dataset: process.env.SANITY_DATASET,
+    },
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/leaflet',
+    'usebootstrap',
+    '@nuxtjs/sanity'
+  ],
+  sanity: {
+    apiVersion: new Date().toISOString().split('T')[0]
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,66 @@
 <template>
-    <div class="container">
-      <div style="height:80vh; width:80vw">
-        <!-- https://github.com/Gugustinette/Nuxt-Leaflet -->
-        <LMap :zoom="zoom" :center="center" :use-global-leaflet="false">
-          <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-            layer-type="base" name="OpenStreetMap" />
-          <LGeoJson :geojson="combined" :options-style="geoStyler" :options="{ onEachFeature }" />
-        </LMap>
+  <div class="container">
+    <h1>Junviglund</h1>
+    <div class="row">
+      <div v-for="p in posts" :key="p._id" class="col-sm-12">
+        <BlogPost :post="p" />
       </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
-  import { L } from 'leaflet'
-  import type { PointExpression, StyleFunction } from 'leaflet'
-  import type { FeatureCollection, Feature } from 'geojson'
-  const zoom = 17
-  const center: PointExpression = [55.4097702, 11.8698327]
-  
-  const matrikel1 = (await useFetch(() => '/api/dataforsyningen/jordstykker?ejerlavkode=211255&matrikelnr=16d')).data.value as FeatureCollection
-  const matrikel2 = (await useFetch(() => '/api/dataforsyningen/jordstykker?ejerlavkode=211255&matrikelnr=17a')).data.value as FeatureCollection
-  const planting = await queryContent('/planting').findOne() as unknown as FeatureCollection
-  const buildings = await queryContent('/buildings').findOne() as unknown as FeatureCollection
-  const planning = await queryContent('/planning').findOne() as unknown as FeatureCollection
-  
-  const combined: FeatureCollection = {
-    type: 'FeatureCollection',
-    features: [
-      ...matrikel1.features,
-      ...matrikel2.features,
-      ...planting.features,
-      ...buildings.features,
-      ...planning.features
-    ]
-  }
-  
-  const onEachFeature = (feature: Feature, layer: L.Layer) => {
-    if (feature?.properties?.name) {
-      const name = feature?.properties?.name ?? 'unknown'
-      const hektar = feature?.properties?.hektar ?? 'unknown'
-      const sorts = feature?.properties?.sorts ?? 'unknown'
-      const l: string[] = []
-      if (Array.isArray(sorts)) {
-        sorts.forEach((sort: string) => {
-          l.push(`<li>${sort}</li>`)
-        })
-        layer.bindPopup(`<h6>${name}</h6> <p>(${hektar} hektar)</p> <ul>${l.join('')}</ul>`)
-      } else {
-        layer.bindPopup(`<h6>${name}</h6>`)
-      }
+  </div>
+</template>
+
+<script setup lang="ts">
+
+type Post = {
+  author: {
+    _ref: string;
+    _type: string;
+  };
+  images: {
+    _type: string;
+    _key: string;
+    asset: {
+      _ref: string;
+      _type: string;
     }
-  }
-  
-  const geoStyler: StyleFunction = (feature) => {
-    return {
-      color: feature?.properties?.stroke ?? '#cccccc',
-      weight: feature?.properties?.['stroke-width'] ?? 1,
-      opacity: feature?.properties?.['stroke-opacity'] ?? 1,
-      fillOpacity: feature?.properties?.['fill-opacity'] ?? 0.5,
-      fillColor: feature?.properties?.fill ?? '#cccccc'
-    }
-  }
-  
-  </script>
+  }[];
+  categories: {
+    _ref: string;
+    _type: string;
+    _key: string;
+  }[];
+  _updatedAt: string;
+  slug: {
+    current: string;
+    _type: string;
+  };
+  title: string;
+  mainImage: {
+    _type: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
+  _createdAt: string;
+  _rev: string;
+  _type: string;
+  _id: string;
+  body: {
+    _type: string;
+    style: string;
+    _key: string;
+    markDefs: any[]; // You can define a type for markDefs if needed
+    children: {
+      _key: string;
+      _type: string;
+      marks: any[]; // You can define a type for marks if needed
+      text: string;
+    }[];
+  }[];
+};
+
+const posts: Post[] = (await useFetch(() => '/api/post')).data.value
+</script>
+
+<style lang="css" scoped></style>
